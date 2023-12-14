2b815aca6448eb6b7026d867636c17848403b5cc
kamilla-t <kamilla-tazieva@mail.ru>
2023-12-15 09:29:22 +1300

    added different languages task

---
+++ different_languages/conftest.py
@@ -0,0 +1,31 @@
+from flask import request
+from selenium import webdriver
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="es", 
+                     help="Choose language: es or ru")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--incognito")
+    user_language = request.config.getoption("language")
+    print(f"\nstart browser with lang {user_language} for test..")
+    
+    if user_language == "es":
+        options.add_experimental_option('prefs', {'intl.accept_languages': 'es-ES'})
+    elif user_language == "ru":
+        options.add_experimental_option('prefs', {'intl.accept_languages': 'ru-RU'})
+    else:
+        raise pytest.UsageError("--language should be es or ru")
+    
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+# вызов 
+# pytest -s -v --language=es test_items.py
---
+++ different_languages/test_items.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.common.by import By
+
+
+def test_guest_should_see_button_add_to_card_pass(browser, request):
+    user_language = request.config.getoption("language")
+    link = f"http://selenium1py.pythonanywhere.com/{user_language}/catalogue/coders-at-work_207/"
+    browser.get(link)
+    browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+
+# def test_guest_should_see_login_link_fail(browser):
+#     browser.get(link)
+#     browser.find_element(By.CSS_SELECTOR, "#magic_link")
+
+
+# Плагины и перезапуск тестов
+# pip install pytest-rerunfailures
+
+# "--reruns n", где n — это количество перезапусков
+# параметр "--tb=line" чтобы сократить лог с результатами теста
+
+# pytest -v --tb=line --reruns 1 --browser_name=chrome test_rerun.py
