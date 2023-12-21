a8f8f6ec86fd9068005515d6b3c0bf783b994881
kamilla-t <kamilla-tazieva@mail.ru>
2023-12-21 14:44:44 +1300

    Реализация LoginPage

---
+++ module/pages/login_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "selenium1py.pythonanywhere.com" in self.browser.current_url, "Url is not correct"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+    #     # def should_be_login_link(self):
+    # assert self.is_element_present(*LoginPageLocators.LOGIN_LINK), "Login link is not presented"
--- module/pages/main_page.py
+++ module/pages/main_page.py
@@ -1,10 +1,18 @@
 from .base_page import BasePage
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from pages.locators import LoginPageLocators
 
 #класс MainPage делаем наследником класса BasePage
 class MainPage(BasePage):
 
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+    # def go_to_login_page(self):
+    #     login_link = self.browser.find_element(By.CSS_SELECTOR, "#registration_link")
+    #     login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_LINK), "Login link is not presented"
+
+
+    # def should_be_login_link(self):
+    #     assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
